3e3cc6f808f273f131e721a11087ad34ba7f1ba9
Matias Arriola <matias.arriola@gmail.com>
2016-12-10 19:46:53 -0300

    fix controls import

--- src/reducers.js
+++ src/reducers.js
@@ -1,6 +1,6 @@
 import {combineReducers} from 'redux'
 import {reducer as config} from './config'
-import {reducer as controls} from './controls'
+import {reducer as controls} from './prefabs/Controls'
 
 export default combineReducers({
   controls,
