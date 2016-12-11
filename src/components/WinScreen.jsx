b44de6e96f86fb1357dcfe528936476293b89270
Matias Arriola <matias.arriola@gmail.com>
2016-12-11 20:25:24 -0300

    Added onda to win screen (?)

--- src/components/WinScreen.jsx
+++ src/components/WinScreen.jsx
@@ -1,9 +1,16 @@
 import React, {Component} from 'react';
 
 export default function WinScreen (props) {
+  const phrase = 'THATS ALL, FOLKS!';
   return (
-    <div>
-      <h1>CONGRATZ!!! YOU MADE IT!</h1>
+    <div className="win-screen">
+      <div className="headers">
+        <h4>{phrase}</h4>
+        <h3>{phrase}</h3>
+        <h2>{phrase}</h2>
+        <h1>{phrase}</h1>
+      </div>
+      <p>:D Thanks for playing :D</p>
     </div>
   );
 }
