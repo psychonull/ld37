aa14daa11ee234a8b7f2bf1ac3a6669251eb7b46
Pablo Novas <pjnovas@gmail.com>
2016-12-12 20:08:30 -0300

    added level 5

--- src/config/levels/index.js
+++ src/config/levels/index.js
@@ -2,5 +2,6 @@ module.exports = [
   {...require('./level1.json')},
   {...require('./level2.json')},
   {...require('./level3.json')},
-  {...require('./level4.json')}
+  {...require('./level4.json')},
+  {...require('./level5.json')}
 ]
