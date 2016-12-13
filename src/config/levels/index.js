54173000925872aa41ebf17e2dfd36025f205b44
Matias Arriola <matias.arriola@gmail.com>
2016-12-12 21:07:48 -0300

    Levels polish

--- src/config/levels/index.js
+++ src/config/levels/index.js
@@ -4,5 +4,7 @@ module.exports = [
   {...require('./level3.json')},
   {...require('./level4.json')},
   {...require('./level5.json')},
-  {...require('./level6.json')}
+  {...require('./level6.json')},
+  {...require('./level7.json')},
+  {...require('./level8.json')},
 ]
